fe3f2eef62430de62579a1c6fc5778d5da9f7986
Henry Gasztowtt <henry.gasztowtt@gmail.com>
2022-08-11 12:58:26 +0200

    reimplemented recursive backtracking as an iterative algorithm

--- src/main/scala/Algorithms/recursiveBacktracking.scala
+++ src/main/scala/Algorithms/recursiveBacktracking.scala
@@ -15,26 +15,46 @@ object RecursiveBacktracking{
 
         // note inverted x and y in tabulate call
         val grid: Grid = Array.tabulate[Set[Direction]](size._2,size._1)((_,_) => Set())
-        visitNextFrom(Point(0,0), grid, size)
-        val maze: Maze = Maze(grid, size)
-        return maze
-    }
 
-    def visitNextFrom(point1: Point, grid: Grid, size: Size): Unit = {
+        val stack = scala.collection.mutable.Stack[Point]()
         val directions: Array[Direction] = Array(N,E,S,W)
-        val randomDirections = scala.util.Random.shuffle(directions)
-        for(direction <- randomDirections){
-            var point2 = move(point1, direction)
-            // if point is valid and has not been visited yet
-            if(isWithin(point2, size) && grid(point2.y)(point2.x).isEmpty){
-                grid(point1.y)(point1.x) += direction
-                grid(point2.y)(point2.x) += opposite(direction)
-                visitNextFrom(point2, grid, size)
+
+        stack.push(Point(0,0))
+
+        while(!stack.isEmpty){
+            var point1 = stack.pop()
+            var randomDirections = scala.util.Random.shuffle(directions)
+
+            // if cell has an unvisited neighbour, find it
+            // and store it and the direction to it in the
+            // appropriately named variables.
+            var foundUnvisitedNeighbour = false
+            var unvisitedNeighbour: Point = null
+            var unvisitedDirection: Direction = null
+            for(direction <- randomDirections){
+                if(!foundUnvisitedNeighbour){
+                    var point2 = move(point1, direction)
+                    // if point is valid and has not been visited yet
+                    if(isWithin(point2, size) && grid(point2.y)(point2.x).isEmpty){
+                        foundUnvisitedNeighbour = true
+                        unvisitedNeighbour = point2
+                        unvisitedDirection = direction
+                    }
+                }
+            }
+
+            // do nothing if cell didn't have an unvisited neighbour,
+            // but if it did then break down the appropriate wall,
+            // then add the original cell back to the stack and 
+            // its neighbour we just visited onto the stack above it.
+            if(foundUnvisitedNeighbour){
+                grid(point1.y)(point1.x) += unvisitedDirection
+                grid(unvisitedNeighbour.y)(unvisitedNeighbour.x) += opposite(unvisitedDirection)
+                stack.push(point1)
+                stack.push(unvisitedNeighbour)
             }
         }
-    }
-
-    def main(args:Array[String]):Unit = {
-        println(mazeToString(RecursiveBacktrackingMazeBuilder(33,15)))
+        val maze: Maze = Maze(grid, size)
+        return maze
     }
 }
